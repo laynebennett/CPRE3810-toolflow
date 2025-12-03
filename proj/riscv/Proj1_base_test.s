f499d8ae211fff2c0680f610229ba81233ca0391
QuinnOM <quinnomahoney@gmail.com>
2025-12-02 21:42:35 -0600

    Create Proj1_base_test.s

---
+++ proj/riscv/Proj1_base_test.s
@@ -0,0 +1,15 @@
+  .text
+    .globl main
+
+main:
+    addi x1,x0,10
+    addi x2,x0,12
+    add  x3,x1,x2
+    sub  x4,x3,x1
+    and  x5,x1,x2
+    or   x6,x1,x2
+    xor  x7,x1,x2
+    sll  x8,x1,x2
+    srl  x9,x8,x2
+    sra  x10,x8,x2
+    slt  x11,x1,x2
